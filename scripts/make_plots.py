f70c4057a75984482eaf8fd81989ee3b710cf70d
Christopher J. White <c.white.astro@gmail.com>
2020-05-17 01:17:09 -0700

    outlined plotting script

---
+++ scripts/make_plots.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+"""
+Script for making all plots.
+
+Datasets:
+  - complex function
+  - magnetized Kelvin-Helmholtz density
+  - magnetized Kelvin-Helmholtz magnetization
+  - poloidal slice of density in GR torus
+  - equatorial slice of current density in GR torus
+  - cosmic microwave background
+
+Colormaps:
+  - viridis
+"""
+
+# Modules
+import numpy as np
+
+# Main function
+def main():
+  pass
+
+# Execute main function
+if __name__ == '__main__':
+  main()
